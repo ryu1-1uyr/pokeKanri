1faacd902d39866e74baf4dc4432647b530f9f8b
s17001 <s17001@std.it-college.ac.jp>
2020-01-21 12:39:06 +0900

    消しちゃった..

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,19 +5,19 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    // parser: 'babel-eslint'
   },
   extends: [
     '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
+    // 'prettier',
+    // 'prettier/vue',
+    // 'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
     '@nuxtjs/eslint-config-typescript'
   ],
-  plugins: [
-    'prettier'
-  ],
+  // plugins: [
+  //   'prettier'
+  // ],
   // add your custom rules here
   rules: {
   }
