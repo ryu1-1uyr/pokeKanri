bdb449efbc60224a5a3272d11d9ef32de879d8dc
s17001 <s17001@std.it-college.ac.jp>
2020-01-21 12:39:15 +0900

    邪魔なやつ消した

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,37 @@
 <template>
-  <div class="container">
+  <section class="container">
     <div>
       <logo />
-      <h1 class="title">
-        pokeKanri
-      </h1>
-      <h2 class="subtitle">
-        My primo Nuxt.js project
-      </h2>
+      <p>{{ age }}</p>
+      <button @click="onClickButton">Click Me!</button>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >Documentation</a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey"
-        >
-          GitHub
-        </a>
+        >GitHub</a>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script lang="ts">
-import Logo from '~/components/Logo.vue'
+import { Component, Vue } from 'vue-property-decorator'
 
-export default {
+@Component({
   components: {
-    Logo
+    Logo: () => import('~/components/Logo.vue')
+  }
+})
+export default class Index extends Vue {
+  age = 1
+  onClickButton () {
+    this.age += 1
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
